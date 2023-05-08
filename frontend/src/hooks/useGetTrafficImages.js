ccebfcabf8e8fcbc92f5930e77ad76270a2b0cb1
David Kwok <davidkwok@ufinity.com>
2023-05-09 04:06:29 +0800

    Fixed issue where certain datetime will have no weather forecast to provide reverse geolocation lookup

--- frontend/src/hooks/useGetTrafficImages.js
+++ frontend/src/hooks/useGetTrafficImages.js
@@ -17,14 +17,22 @@ export const useGetTrafficImages = (selectedDatetime, weatherData) => {
       const traffic_json = await traffic_res.json();
       if (traffic_json?.items[0]?.cameras) {
         traffic_json.items[0].cameras.forEach(camera => {
-          //find the nearest area 
-          camera.area = weatherData.area_metadata.reduce((acc, area) => {
-            const accDist = calcCoordinatesDists(acc.label_location, camera.location);
-            const areaDist = calcCoordinatesDists(area.label_location, camera.location);
-            return areaDist < accDist ? area : acc;
-          });
-          const forecastData = weatherData.items[0].forecasts.find((area)=>area.area === camera.area.name);
-          camera.area.forecast = forecastData ? forecastData.forecast : "NODATA";
+          if (Array.isArray(weatherData?.area_metadata) &&
+              weatherData?.area_metadata.length > 0) {
+            //find the nearest area 
+            camera.area = weatherData.area_metadata.reduce((acc, area) => {
+              const accDist = calcCoordinatesDists(acc.label_location, camera.location);
+              const areaDist = calcCoordinatesDists(area.label_location, camera.location);
+              return areaDist < accDist ? area : acc;
+            });
+          } else {
+            camera.area = {
+              name: 'Singapore'
+            }
+          }
+          console.log(camera);
+          const forecastData = weatherData?.items[0]?.forecasts?.find((area)=>area.area === camera.area.name);
+          camera.area.forecast = forecastData ? forecastData.forecast : "";
         });
         traffic_json.items[0].cameras =
           traffic_json.items[0].cameras.toSorted((a, b) => a.camera_id - b.camera_id);
