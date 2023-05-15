af75d7e65eb46d22c5f7a2c46c43b46cae988afd
David Kwok <davidkwok@ufinity.com>
2023-05-15 19:42:32 +0800

    Implemented retries for weather to get previous days of area metadata in the event current datetime one is empty. This allow populating the area name, but remove weather information

--- frontend/src/hooks/useGetWeather.js
+++ frontend/src/hooks/useGetWeather.js
@@ -1,7 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
 import { WEATHER_FORECAST_URL, WEATHER_FORECAST_KEY } from "utils/Constants";
+import dayjs from 'dayjs';
 
 export const useGetWeather = (selectedDatetime) =>  {
+
+  const origSelectedDatetime = selectedDatetime;
+
+
   return useQuery({
     queryKey: [WEATHER_FORECAST_KEY, {selectedDatetime}],
     queryFn: () => fetch(`${WEATHER_FORECAST_URL}?${new URLSearchParams({
@@ -12,8 +17,24 @@ export const useGetWeather = (selectedDatetime) =>  {
           "Content-Type": "application/json"
         }
       })
-      .then(res => res.json()),
+      .then(res => res.json())
+      .then(obj => {
+        if (origSelectedDatetime !== selectedDatetime) {
+          // remove the weather forecast and only retain the area metadata because the weather
+          // information are no longer accurate
+          obj.items = [];
+        }
+        if (!(Array.isArray(obj?.area_metadata) && obj.area_metadata.length > 0))
+          throw new Error("Missing weather area metadata");
+        return obj;
+      }),
       staleTime: Infinity,
-      cacheTime: Infinity
+      cacheTime: Infinity,
+      retry: (failureCount, error) => {
+        selectedDatetime = dayjs(dayjs(selectedDatetime).subtract(1, 'day')).format("YYYY-MM-DDTHH:mm:ssZ");
+        console.log("Retrying for date " + selectedDatetime);
+        return failureCount < 12;
+      },
+      retryDelay: 100
   })
 }
